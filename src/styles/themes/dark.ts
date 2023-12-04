3b4be80842e5a2369cd1a9280a0c2b1a09487208
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-04 18:45:21 -0300

    Set up dark and light theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,23 @@
+const darkTheme = {
+  title: "dark",
+  colors: {
+    main: {
+      background: "#030303", // Dark background for the main background
+      text: "#fff", // White text color
+      accent: "#99C2FF", // Accent blue for main elements
+    },
+    secondary: {
+      background: "#454545", // Dark background for secondary background
+      text: "#fff", // White text color
+      accent: "#66A3FF", // Accent blue for secondary elements
+    },
+    text: {
+      background: "#1A1A1A", // Dark background for text
+      color: "#fff", // White text color
+      dim: "#959595", // Dim white for less important text
+      important: "#000", // Black for important text
+    },
+  },
+};
+
+export default darkTheme;
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,23 @@
+const lightTheme = {
+  title: "light",
+  colors: {
+    main: {
+      background: "#E6F7FF", // Light blue for the main background
+      text: "#1A1A1A", // Dark blue for text color
+      accent: "#66A3FF", // Accent blue for main elements
+    },
+    secondary: {
+      background: "#B3D9FF", // Light blue for secondary background
+      text: "#1A1A1A", // Dark blue for text color
+      accent: "#99C2FF", // Accent blue for secondary elements
+    },
+    text: {
+      background: "#fff", // White background for text
+      color: "#1A1A1A", // Dark blue for text color
+      dim: "#6B6B6B", // Dim blue for less important text
+      important: "#000", // Black for important text
+    },
+  },
+};
+
+export default lightTheme;

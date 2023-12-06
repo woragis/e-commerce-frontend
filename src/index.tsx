e5f541ef60a5099434001d19974325966f02dfc5
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-05 23:21:51 -0300

    Create global styles and modified index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import App from "./app/App";
+import GlobalStyles from "./styles/GlobalStyles";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <GlobalStyles />
+    <App />
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -5,6 +5,9 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    list-style-type: none;
+    text-decoration: none;
+    transition: 1s;
   }
 `;
 

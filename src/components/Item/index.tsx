84d3f6ce96582364ec9e43e892b5cb92b59c636e
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-05 23:10:18 -0300

    Improve store item component

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,7 +1,20 @@
+import { FaPlus, FaMinus } from "react-icons/fa6";
 import { StyledStoreItem } from "./style";
 
-export const CartItem = () => {
-  return <div>Item</div>;
+export const CartItem = ({ name, price, old_price }: storeItemProps) => {
+  return (
+    <div>
+      <p className='item-name'>{name.toUpperCase()}</p>
+      <p className='price'>{price}</p>
+      <button>
+        Add <FaPlus />
+      </button>
+      <button>
+        Minus <FaMinus />
+      </button>
+      <button>Buy Alone</button>
+    </div>
+  );
 };
 
 interface storeItemProps {
@@ -13,6 +26,7 @@ export const StoreItem = ({ name, price, old_price }: storeItemProps) => {
   const discount = Math.floor((Number(price) / Number(old_price)) * 100);
   return (
     <StyledStoreItem>
+      <img src='#' alt={"Image of " + name} className='item-img' />
       <p className='item-name'>{name}</p>
       <p className='item-old-price'>R$ {old_price}</p>
       <p className='item-price'>R$ {price}</p>
--- src/components/Item/style.ts
+++ src/components/Item/style.ts
@@ -2,9 +2,22 @@ import styled from "styled-components";
 
 export const StyledStoreItem = styled.div`
   border: 1px solid gray;
+  font-family: "Hedvig Letters Serif", serif;
 
-  width: 300px;
+  width: 180px;
+  height: 260px;
   margin: 0 auto;
+  padding: 5px;
+  border-radius: 5px;
+  box-shadow: 0.5px 0.5px 4px ${props => props.theme.text.color};
+
+  .item-img {
+    display: block;
+    height: 130px;
+    border: 1px solid black;
+    width: 100%;
+  }
+
   .item-name {
     font-weight: 700;
     font-size: 1.2em;

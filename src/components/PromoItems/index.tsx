e9458ca9000a19f71d2a85af5faaf613097d0256
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2023-12-06 21:24:52 -0300

    Add rgb border to Promo items

--- src/components/PromoItems/index.tsx
+++ src/components/PromoItems/index.tsx
@@ -1,23 +1,44 @@
 import { FC } from "react";
 import { promoItemProps } from "../../types/items.type";
 import { PromoContainer, StyledPromoItem } from "./style";
+import { motion } from "framer-motion";
+import { useSpring, animated, config } from "react-spring";
+import { Link } from "react-router-dom";
 
-const PromoItem: FC<promoItemProps> = ({ title, img }: promoItemProps) => {
+const PromoItem: FC<promoItemProps> = ({ id, title, img }: promoItemProps) => {
   return (
     <StyledPromoItem>
-      <img src={img} alt={title} />
+      <Link to={"/item/" + id}>
+        <div className='rgb-container'>
+          <div className='promo-item'>
+            <img src={img} alt={title} />
+          </div>
+        </div>
+      </Link>
     </StyledPromoItem>
   );
 };
 
 const PromoItems: FC = () => {
-  return (
-    <PromoContainer>
-      <PromoItem title='Computador Gamer de gratis, no seu cu' img='www.www' />
-      <PromoItem title='Playstation 5 de gratis no seu cu, olhe para tras' img='www.www' />
-      <PromoItem title='Xbox Series X de gratis por apenas 5000 miu reais (TA PORRAAAH)' img='www.www' />
-    </PromoContainer>
-  );
+  const discounts = [
+    { id: 0, title: "Playstation 5", img: "www" },
+    { id: 1, title: "Playstation 4", img: "www" },
+    { id: 2, title: "Playstation 3", img: "www" },
+    { id: 3, title: "Playstation 2", img: "www" },
+    { id: 4, title: "Playstation 1", img: "www" },
+    { id: 5, title: "Xbox Series X", img: "www" },
+    { id: 6, title: "Xbox Series S", img: "www" },
+    { id: 7, title: "Xbox One X", img: "www" },
+    { id: 8, title: "Xbox One slim", img: "www" },
+    { id: 9, title: "Xbox One", img: "www" },
+  ];
+  const promoItems = discounts.map(({ id, title, img }, index) => {
+    if (index > 2) {
+      return;
+    }
+    return <PromoItem key={id} id={id} title={title} img={img} />;
+  });
+  return <PromoContainer>{promoItems}</PromoContainer>;
 };
 
 export default PromoItems;
--- src/components/PromoItems/style.ts
+++ src/components/PromoItems/style.ts
@@ -1,27 +1,129 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { darken, lighten, shade } from "polished";
 
 export const PromoContainer = styled.section`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  justify-content: center;
+  /* background-image: linear-gradient(
+    ${props => props.theme.background.color.neutral},
+    ${props => lighten(0.3, props.theme.background.color.neutral)},
+    ${props => darken(0.5, props.theme.background.color.main)},
+    ${props => props.theme.background.color.main},
+    ${props => lighten(0.3, props.theme.background.color.neutral)},
+    ${props => props.theme.background.color.neutral}
+  ); */
+  /* background-image: radial-gradient(red, blue); */
+  place-items: center;
   align-items: center;
   grid-gap: 20px;
-  border: 1px solid red;
+  /* border: 1px solid red; */
   width: 100%;
-  height: 180px;
+  height: 150px;
   padding: 15px;
+  position: relative;
+  &::before {
+    content: "";
+    position: absolute;
+    top: -50%;
+    left: 0;
+    transform: translate(-50%, 0);
+    transform: scale(200%, 180%);
+    width: 100%;
+    height: 100%;
+    /* z-index: 1; */
+    background-image: radial-gradient(
+      ${props => props.theme.background.color.main},
+      ${props => props.theme.background.color.neutral},
+      ${props => props.theme.background.color.neutral}
+    );
+    backdrop-filter: blur(10px);
+  }
+`;
+
+const rotationAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 `;
 
 export const StyledPromoItem = styled.article`
-  height: 150px;
-  width: 400px;
-  border: 1px solid black;
-  font-size: 2em;
-  text-shadow: 3px 3px 3px lightblue;
-  margin: 0 auto;
-  color: ${props => props.theme.text.color};
-  background-color: ${props => props.theme.background.color.main};
-  background-image: url("ww.wwu.");
-  background-repeat: no-repeat;
-  background-size: cover;
+  .promo-item {
+    height: 100px;
+    width: 300px;
+    font-size: 1.3em;
+    /* text-shadow: 3px 3px 3px ${props => props.theme.background.color.secondary}; */
+    margin: 0 auto;
+    border: 3px solid turquoise;
+    color: ${props => props.theme.text.color};
+    background-image: url("ww.wwu.");
+    background-repeat: no-repeat;
+    background-size: cover;
+    z-index: 5;
+  }
+  .rgb-container {
+    width: 310px;
+    height: 110px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    background-color: ${props => props.theme.background.color.neutral};
+    overflow: hidden;
+    box-shadow: 0 0 15px ${props => lighten(0, props.theme.background.color.secondary)};
+    transition: 300ms;
+
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      z-index: 0;
+    }
+    &::before {
+      top: -50%;
+      left: -50%;
+      width: 600px;
+      height: 600px;
+      background: linear-gradient(to right, violet, indigo, blue, green, yellow, orange, red, pink);
+      animation: rotationAnimation 2s linear infinite;
+      animation-name: ${rotationAnimation};
+      animation-duration: 15s;
+      animation-timing-function: linear;
+      animation-delay: 0ms;
+      animation-iteration-count: infinite;
+      animation-direction: normal;
+    }
+    &:hover {
+      transform: scale(110%);
+      &::before {
+        animation-duration: 1.5s;
+        animation-direction: reverse;
+      }
+    }
+    &::after {
+      height: 100px;
+      width: 300px;
+      background-color: ${props => props.theme.background.color.neutral};
+    }
+  }
+
+  /* .rgb-container {
+    height: 105px;
+    width: 305px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    z-index: -5;
+  }
+  .rgb-background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    transform-origin: center;
+    background: linear-gradient(to right, violet, indigo, blue, green, yellow, orange, red, pink);
+    z-index: -5;
+  } */
 `;
